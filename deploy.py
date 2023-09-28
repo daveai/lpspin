3c7f0af0a4abc486f5831104441d9ad2d175ea56
daveai <dave@pm.me>
2023-09-28 17:48:04 +0100

    migrate to web3py v6

--- deploy.py
+++ deploy.py
@@ -26,7 +26,7 @@ def fetch_price(address):
         )
     )
     # Get the price from the response
-    price = req.json()['result']["usd"]
+    price = req.json()[address.lower()]["usd"]
     return price
 
 
@@ -40,19 +40,19 @@ def approve(token, amount):
     erc20 = w3.eth.contract(address=token, abi=erc20_abi)
     tx = erc20.functions.approve(
         "0xAE8fbE656a77519a7490054274910129c9244FA3", amount
-    ).buildTransaction(
+    ).build_transaction(
         {
             "from": acc.address,
-            "nonce": w3.eth.getTransactionCount(w3.eth.accounts[0]),
+            "nonce": w3.eth.get_transaction_count(acc.address),
             "gas": 100000,
-            "gasPrice": w3.toWei("5", "gwei"),
+            "gasPrice": w3.to_wei("5", "gwei"),
         }
     )
     signed_tx = w3.eth.account.sign_transaction(
         tx, private_key=os.environ["PRIVATE_KEY"]
     )
-    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    print(w3.eth.waitForTransactionReceipt(tx_hash))
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    print(w3.eth.wait_for_transaction_receipt(tx_hash))
 
 
 if __name__ == "__main__":
@@ -63,52 +63,66 @@ if __name__ == "__main__":
     token0 = p["tokens"][0]
     token1 = p["tokens"][1]
     assert token0["address"] < token1["address"], "Token addresses are not in order"
-    
+
     # Fetch price
     price0 = fetch_price(token0["address"])
     price1 = fetch_price(token1["address"])
-    
+
     # Calculate the price ratio
     price_ratio = price0 / price1
     # Calculate the sqrtPriceX96
-    sqrt_price = sqrtPriceX96(price_ratio)
-    
+    sqrt_price = int(sqrtPriceX96(price_ratio))
+
     # Amounts to LP
-    amount0 = price0 / token0["amountUSD"]
-    amount1 = price1 / token1["amountUSD"]
-    
+    amount0 = int(price0 / token0["amountUSD"] * 10**18)
+    amount1 = int(price1 / token1["amountUSD"] * 10**18)
+
     # Approve the tokens
-    approve(token0["address"], amount0)
-    approve(token1["address"], amount1)
-    
-    uni.functions.createAndInitializePoolIfNecessary(token0, token1, p["fee"], sqrt_price).buildTransaction(
+    approve(token0["address"], 115792089237316195423570985008687907853269984665640564039457584007913129639935)
+    approve(token1["address"], 115792089237316195423570985008687907853269984665640564039457584007913129639935)
+
+    create = uni.functions.createAndInitializePoolIfNecessary(
+        token0['address'], token1['address'], p["fee"], sqrt_price
+    ).build_transaction(
         {
             "from": acc.address,
-            "nonce": w3.eth.getTransactionCount(w3.eth.accounts[0]),
+            "nonce": w3.eth.get_transaction_count(acc.address),
             "gas": 7000000,
-            "gasPrice": w3.toWei("5", "gwei"),
+            "gasPrice": w3.to_wei("5", "gwei"),
         }
     )
     signed_tx = w3.eth.account.sign_transaction(
-        tx, private_key=os.environ["PRIVATE_KEY"]
+        create, private_key=os.environ["PRIVATE_KEY"]
     )
-    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    print(w3.eth.waitForTransactionReceipt(tx_hash))
-    
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    print(w3.eth.wait_for_transaction_receipt(tx_hash))
+
     t_10 = datetime.now() + timedelta(minutes=10)
     timestamp = int(t_10.timestamp())
 
-    uni.functions.mint(token0, token1, p["fee"], p["tickLower"], p["tickUpper"] amount0, amount1, amount0*0.98, amount1*0.98, acc.address, timestamp).buildTransaction(
+    mint = uni.functions.mint(
+        (token0['address'],
+        token1['address'],
+        p["fee"],
+        p["tickLower"],
+        p["tickUpper"],
+        amount0,
+        amount1,
+        int(amount0 * 0.98),
+        int(amount1 * 0.98),
+        acc.address,
+        timestamp)
+    ).build_transaction(
         {
             "from": acc.address,
-            "nonce": w3.eth.getTransactionCount(w3.eth.accounts[0]),
+            "nonce": w3.eth.get_transaction_count(acc.address),
             "gas": 2000000,
-            "gasPrice": w3.toWei("5", "gwei"),
+            "gasPrice": w3.to_wei("5", "gwei"),
         }
     )
     signed_tx = w3.eth.account.sign_transaction(
-        tx, private_key=os.environ["PRIVATE_KEY"]
+        mint, private_key=os.environ["PRIVATE_KEY"]
     )
-    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    print(w3.eth.waitForTransactionReceipt(tx_hash))
     
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    print(w3.eth.wait_for_transaction_receipt(tx_hash))
